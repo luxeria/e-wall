a024e814a2f24a34f51669d8da01428be480ca0f
daniw <daniel.winz@stud.hslu.ch>
2013-08-24 19:34:35 +0200

    Datenblatt und Family Guide MSP430G2553

--- datasheets/datasheets.sh
+++ datasheets/datasheets.sh
@@ -1,3 +1,10 @@
 #/bin/bash
+
+# 74HC595 / 74HCT595
 wget http://www.parallax.com/Portals/0/Downloads/docs/prod/datast/74HC_HCT595_4.pdf
 
+# MSP430 Family Guide
+wget http://www.ti.com/lit/ug/slau144j/slau144j.pdf
+
+# MSP430G2553
+wget http://www.ti.com/lit/ds/symlink/msp430g2553.pdf
