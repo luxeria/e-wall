91c72ebf84d99c5b802c4e169adff2cec651c91c
daniw <dani.winz@bluewin.ch>
2014-04-20 21:02:52 +0200

    ereutes herunterladen von bereits vorhandenen Datenblättern unterdrückt

--- datasheets/datasheets.sh
+++ datasheets/datasheets.sh
@@ -1,13 +1,13 @@
 #/bin/bash
 
 # 74HC Family Specification
-wget http://web.mit.edu/6.111/www/s2007/datasheets/hctfamspec.pdf
+wget -nc http://web.mit.edu/6.111/www/s2007/datasheets/hctfamspec.pdf
 
 # 74HC595 / 74HCT595
-wget http://www.parallax.com/Portals/0/Downloads/docs/prod/datast/74HC_HCT595_4.pdf
+wget -nc http://www.parallax.com/Portals/0/Downloads/docs/prod/datast/74HC_HCT595_4.pdf
 
 # MSP430 Family Guide
-wget http://www.ti.com/lit/ug/slau144j/slau144j.pdf
+wget -nc http://www.ti.com/lit/ug/slau144j/slau144j.pdf
 
 # MSP430G2553
-wget http://www.ti.com/lit/ds/symlink/msp430g2553.pdf
+wget -nc http://www.ti.com/lit/ds/symlink/msp430g2553.pdf
