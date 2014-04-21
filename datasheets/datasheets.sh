e3e3731f63466fbf76e78bdd13ae72903d939b81
daniw <dani.winz@bluewin.ch>
2014-04-21 08:02:32 +0200

    Datenblätter der Bipolartransistoren

--- datasheets/datasheets.sh
+++ datasheets/datasheets.sh
@@ -14,3 +14,8 @@ wget -nc http://www.ti.com/lit/ds/symlink/msp430g2553.pdf
 
 # ULN2803
 wget -nc -O ULN2803A.pdf http://www.st.com/web/en/resource/technical/document/datasheet/CD00000179.pdf
+
+# Transistors
+wget -nc http://www.nxp.com/documents/data_sheet/BC847_BC547_SER.pdf
+wget -nc http://www.nxp.com/documents/data_sheet/BC556_557.pdf
+
